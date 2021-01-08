ed752194bb10e43a1f38f975efd98d6d37b1b5b0
linmy1998 <2388422804@qq.com>
2021-01-08 15:17:43 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+  
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+@import 'assets/css/base.css';
+</style>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports={
+    configurewebpack:{
+        resolve:{
+            alias: {
+                'assets':'@/assets',
+                'common':'@/common',
+                'components':'@/components',
+                'network':'@/network',
+                'views':'@/views',
+            }
+        }
+    }
+}
